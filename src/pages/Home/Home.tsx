166004b13b0401a01ad1955e6ab3d8c296a93f80
teonapiranishvili <100440747+teonateona12@users.noreply.github.com>
2023-08-22 05:50:41 -0700

    Teona footer (#8)
    * setup
    
    * feat: add footer

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,7 @@
+import Footer from "@/components/Footer/Footer";
+
+export default function Home() {
+  return <div><Footer/></div>;
 import { Header, TopSection } from "../../components";
 
 export default function Home() {
