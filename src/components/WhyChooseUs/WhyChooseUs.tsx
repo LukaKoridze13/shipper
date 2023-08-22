651541ef032383a243ca7eb1f59819b2037aee94
teonapiranishvili <100440747+teonateona12@users.noreply.github.com>
2023-08-22 06:57:29 -0700

    Teona (#15)
    * setup
    
    * docs: install react router

--- src/components/WhyChooseUs/WhyChooseUs.tsx
+++ src/components/WhyChooseUs/WhyChooseUs.tsx
@@ -2,7 +2,7 @@ import whyChooseUsLogo from "../../Images/whyChooseUs.svg";
 
 function WhyChooseUs() {
   return (
-    <div className="flex justify-center items-start w-auto pt-56 pl-16 pr-16 pb-60">
+    <div className="flex justify-center items-start w-auto pt-56 pl-16 pr-16">
       <div className="max-w-[751px]">
         <h1 className="text-3xl font-medium leading-normal text-black pb-9">Why Choose Us?</h1>
         <p className="text-2xl font-normal text-black pt-5 max-w-[751px]">
