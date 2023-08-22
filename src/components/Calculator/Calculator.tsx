3ced25245de0432dee35972b21f3edeb3982e897
Tusho7 <sandrotushurashvili7@gmail.com>
2023-08-22 05:41:05 -0700

    feat: Create WhyChooseUs.tsx

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,4 +1,4 @@
-import {useCalculator} from "../../hooks"
+import useCalculator from "../Calculator/useCalculator"
 function Calculator() {
 
   const {data,heightCm,onClick,lengthCm,widthCm,weightKg, setLengthCm,setHeightCm,setWidthCm,setWeightKg  } = useCalculator()
