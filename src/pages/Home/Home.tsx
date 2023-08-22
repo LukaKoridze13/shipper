bc0210bf183c738143545512dab65373c439b1d0
teonapiranishvili <100440747+teonateona12@users.noreply.github.com>
2023-08-22 06:16:10 -0700

    Fix bugs (#10)
    * setup
    
    * fix: bugs

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
+import WhyChooseUs from "../../components/WhyChooseUs/WhyChooseUs";
 import TopSection from "../../components/TopSection/TopSection";
 
 export default function Home() {
   return (
     <>
       <TopSection />
+      <WhyChooseUs/>
     </>
   );
 }
