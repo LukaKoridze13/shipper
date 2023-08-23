70a08da10c7e000d17cf5de885c3d43a48972506
anrikopaliani <anrikopa@gmail.com>
2023-08-23 08:44:08 +0100

    feat:create useLogin hook

---
+++ src/pages/LogIn/useLogin.ts
@@ -0,0 +1,34 @@
+import usePasswordToggle from "@/hooks/usePasswordToggle";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const useLogin = () => {
+    const [showPassword, togglePasswordVisibility] = usePasswordToggle();
+
+    const schema = yup.object().shape({
+      email: yup.string().required("required"),
+      password: yup.string().required("required"),
+    });
+  
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm({
+      resolver: yupResolver(schema),
+      defaultValues: {
+        password: "",
+        email: "",
+      },
+    });
+    const onSubmit = (data: any) => {
+      console.log(data);
+    };
+
+    return {
+        showPassword,togglePasswordVisibility,register, handleSubmit, errors, onSubmit
+    }
+}
+
+export default useLogin
